35dc6d38cc8652d2b7d5451c0acbf3a8e3d79db4
Erik C. Thauvin <erik@thauvin.net>
2024-08-28 18:29:26 -0700

    Bumped PIT to version 1.16.2

--- examples/src/bld/java/com/example/ExamplesBuild.java
+++ examples/src/bld/java/com/example/ExamplesBuild.java
@@ -27,8 +27,8 @@ public class ExamplesBuild extends Project {
         repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES);
 
         scope(test)
-                .include(dependency("org.pitest", "pitest", version(1, 16, 1)))
-                .include(dependency("org.pitest", "pitest-command-line", version(1, 16, 1)))
+                .include(dependency("org.pitest", "pitest", version(1, 16, 2)))
+                .include(dependency("org.pitest", "pitest-command-line", version(1, 16, 2)))
                 .include(dependency("org.pitest", "pitest-junit5-plugin", version(1, 2, 1)))
                 .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 11, 0)))
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 11, 0)));
