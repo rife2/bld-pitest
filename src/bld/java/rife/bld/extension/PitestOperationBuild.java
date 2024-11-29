11bdafa259949b7191b8464fa79e70f42bee9d81
Erik C. Thauvin <erik@thauvin.net>
2024-11-29 08:34:58 -0800

    Version 1.0.4

--- src/bld/java/rife/bld/extension/PitestOperationBuild.java
+++ src/bld/java/rife/bld/extension/PitestOperationBuild.java
@@ -38,7 +38,7 @@ public class PitestOperationBuild extends Project {
     public PitestOperationBuild() {
         pkg = "rife.bld.extension";
         name = "PitestExtension";
-        version = version(1, 0, 3);
+        version = version(1, 0, 4);
 
         javaRelease = 17;
 
