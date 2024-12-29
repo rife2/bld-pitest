d49fc24bd7646a9afec3fd2304fa70eded8ebe03
Erik C. Thauvin <erik@thauvin.net>
2024-12-28 18:06:41 -0800

    Bumped AssertJ to version 3.27.0

--- src/bld/java/rife/bld/extension/PitestOperationBuild.java
+++ src/bld/java/rife/bld/extension/PitestOperationBuild.java
@@ -56,7 +56,7 @@ public class PitestOperationBuild extends Project {
                 .include(dependency("org.pitest", "pitest-junit5-plugin", version(1, 2, 1)))
                 .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 11, 4)))
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 11, 4)))
-                .include(dependency("org.assertj", "assertj-core", version(3, 26, 3)));
+                .include(dependency("org.assertj", "assertj-core", version(3, 27, 0)));
 
         javadocOperation()
                 .javadocOptions()
