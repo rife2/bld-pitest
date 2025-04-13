bf1c68d304e0cd9a94d8ed3ee272d6063741b7d9
Erik C. Thauvin <erik@thauvin.net>
2025-04-12 21:02:45 -0700

    Bump JUnit to version 5.12.2

--- examples/src/bld/java/com/example/ExamplesBuild.java
+++ examples/src/bld/java/com/example/ExamplesBuild.java
@@ -34,8 +34,8 @@ public class ExamplesBuild extends Project {
                 .include(dependency("org.pitest", "pitest", pitest))
                 .include(dependency("org.pitest", "pitest-command-line", pitest))
                 .include(dependency("org.pitest", "pitest-junit5-plugin", version(1, 2, 2)))
-                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 12, 1)))
-                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 12, 1)));
+                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 12, 2)))
+                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 12, 2)));
     }
 
     public static void main(String[] args) {
--- src/bld/java/rife/bld/extension/PitestOperationBuild.java
+++ src/bld/java/rife/bld/extension/PitestOperationBuild.java
@@ -54,8 +54,8 @@ public class PitestOperationBuild extends Project {
                 .include(dependency("org.pitest", "pitest", pitest))
                 .include(dependency("org.pitest", "pitest-command-line", pitest))
                 .include(dependency("org.pitest", "pitest-junit5-plugin", version(1, 2, 2)))
-                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 12, 1)))
-                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 12, 1)))
+                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 12, 2)))
+                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 12, 2)))
                 .include(dependency("org.assertj", "assertj-core", version(3, 27, 3)));
 
         javadocOperation()
