c2349f4d9acef2994d3610ad22d2305b9ed99abf
Erik C. Thauvin <erik@thauvin.net>
2025-06-05 08:31:20 -0700

    Bump PIT to version 1.19.5

--- examples/src/bld/java/com/example/ExamplesBuild.java
+++ examples/src/bld/java/com/example/ExamplesBuild.java
@@ -29,13 +29,13 @@ public class ExamplesBuild extends Project {
 
         repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES);
 
-        var pitest = version(1, 19, 4);
+        var pitest = version(1, 19, 5);
         scope(test)
                 .include(dependency("org.pitest", "pitest", pitest))
                 .include(dependency("org.pitest", "pitest-command-line", pitest))
                 .include(dependency("org.pitest", "pitest-junit5-plugin", version(1, 2, 3)))
-                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 12, 2)))
-                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 12, 2)));
+                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 13, 0)))
+                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 13, 0)));
     }
 
     public static void main(String[] args) {
--- src/bld/java/rife/bld/extension/PitestOperationBuild.java
+++ src/bld/java/rife/bld/extension/PitestOperationBuild.java
@@ -50,15 +50,15 @@ public class PitestOperationBuild extends Project {
 
         repositories = List.of(MAVEN_LOCAL, MAVEN_CENTRAL, RIFE2_RELEASES, RIFE2_SNAPSHOTS);
 
-        var pitest = version(1, 19, 4);
+        var pitest = version(1, 19, 5);
         scope(compile)
                 .include(dependency("com.uwyn.rife2", "bld", version(2, 2, 1)));
         scope(test)
                 .include(dependency("org.pitest", "pitest", pitest))
                 .include(dependency("org.pitest", "pitest-command-line", pitest))
                 .include(dependency("org.pitest", "pitest-junit5-plugin", version(1, 2, 3)))
-                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 12, 2)))
-                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 12, 2)))
+                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 13, 0)))
+                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 13, 0)))
                 .include(dependency("org.assertj", "assertj-core", version(3, 27, 3)));
 
         javadocOperation()
