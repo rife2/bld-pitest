464e346d41eae7b3c10a1f1f2261635ae3c44269
Erik C. Thauvin <erik@thauvin.net>
2025-07-11 23:12:29 -0700

    Bump JUnit to version 5.13.3

--- src/bld/java/rife/bld/extension/PitestOperationBuild.java
+++ src/bld/java/rife/bld/extension/PitestOperationBuild.java
@@ -57,8 +57,8 @@ public class PitestOperationBuild extends Project {
                 .include(dependency("org.pitest", "pitest", pitest))
                 .include(dependency("org.pitest", "pitest-command-line", pitest))
                 .include(dependency("org.pitest", "pitest-junit5-plugin", version(1, 2, 3)))
-                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 13, 2)))
-                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 13, 2)))
+                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 13, 3)))
+                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 13, 3)))
                 .include(dependency("org.assertj", "assertj-core", version(3, 27, 3)));
 
         javadocOperation()
